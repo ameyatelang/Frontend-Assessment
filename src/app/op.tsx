8cf19bf0434ce044a8363e252f0f7c1544a8d420
ameyat <at156@rice.edu>
2025-12-12 14:34:41 -0600

    Full changes + bonus

--- src/app/op.tsx
+++ src/app/op.tsx
@@ -1,12 +1,14 @@
 "use client"
 
 import React, {useEffect, useState} from "react";
-import {Table, Typography, Card, CardContent,
-    TableContainer, TableRow, TableCell,
-    TableHead, TableBody, Chip, Button,
-    createTheme, ThemeProvider} from "@mui/material";
+import {Typography, Card, CardContent, Chip, Button, createTheme, ThemeProvider, TextField} from "@mui/material";
+import DataTable, { TableColumn } from "react-data-table-component";
+import './op.css';
 
 
+/**
+ * Extracting needed fields for each Op
+ */
 interface Op{
     opId: number,
     opTitle: string,
@@ -17,6 +19,9 @@ interface Op{
     operators: Operator[]
 }
 
+/**
+ * Extracting needed fields for each Operator
+ */
 interface Operator{
     id: number,
     firstName: string,
@@ -26,6 +31,10 @@ interface Operator{
     endorsements: string[]
 }
 
+/**
+ * Converts date string into readable format
+ * @param date
+ */
 export function dateFormat(date: string): string{
     const newDate = new Date(date);
     return newDate.toLocaleString('en-US', {
@@ -37,27 +46,43 @@ export function dateFormat(date: string): string{
     })
 }
 
+//Setting the theme for MUI elements with chosen font
 const font = createTheme({
     typography: {fontFamily: 'Montserrat, sans-serif'}
 })
 
-const GetOp: React.FC = () =>{
+/**
+ * React component for MUI cards displaying each Op and relevant info
+ * @param filter
+ * @constructor
+ */
+const GetOp: React.FC<{filter: string}> = ({filter}) =>{
 
     const [op, setOp] = useState<Op[]>([]);
-    const [workerCheckIn, setWorkerCheckIn] = useState<Record<string, {checkIn: string, checkOut: string}>>({});
+    const [workerCheckIn, setWorkerCheckIn] = useState<Record<string, {checkIn: string, checkOut: string, buttonColor: boolean}>>({});
 
+    /**
+     * Handler for check in/out functionality per operator
+     *
+     * @remarks
+     *Based on the state of the checkIn and checkOut strings
+     * the handler returns the record with when the button was clicked
+     * and changes the color
+     *
+     * @param workerId
+     */
     const handleCheckIn = (workerId: number) =>{
         setWorkerCheckIn((opr)=>{
             const curOpr = opr[workerId] || {};
             if(!curOpr.checkIn){
                 return{
                     ...opr,
-                    [workerId]: {checkIn: dateFormat(new Date().toLocaleString())}
+                    [workerId]: {checkIn: dateFormat(new Date().toLocaleString()), buttonColor: true}
                 }
             }else if(!curOpr.checkOut){
                 return{
                     ...opr,
-                    [workerId]: {...curOpr, checkOut: dateFormat(new Date().toLocaleString())}
+                    [workerId]: {...curOpr, checkOut: dateFormat(new Date().toLocaleString()), buttonColor: false}
                 }
             }else{
                 return opr;
@@ -65,6 +90,20 @@ const GetOp: React.FC = () =>{
         });
     }
 
+    /**
+     * Filter logic to filter the cards by opTitle, publicId, or operators
+     * when typed into MUI textField
+     */
+    const filterListings = op.filter(
+        (card)=> card.opTitle.toLowerCase().includes(filter.toLowerCase()) ||
+            card.publicId.includes(filter) ||
+             card.operators.some((findOpr) =>
+                (findOpr.firstName + " " + findOpr.lastName).toLowerCase().includes(filter.toLowerCase()))
+    )
+
+    /**
+     * Fetching the JSON data from the endpoint
+     */
     useEffect(() => {
         fetch("https://frontend-challenge.veryableops.com/")
             .then((payload) => payload.json())
@@ -72,59 +111,126 @@ const GetOp: React.FC = () =>{
             .catch((error) => console.error(error))
     },[])
 
+    /**
+     * Storing operatorID and timestamps of checkIn and checkOut
+     * to local storage
+     */
     useEffect(() => {
         localStorage.setItem("Check In/Out Timings", JSON.stringify(workerCheckIn));
     }, [workerCheckIn]);
 
+    /**
+     * Defining all the columns to display operator info
+     * Name, Ops Completed, and Reliability are all sortable
+     */
+    const cols: TableColumn<Operator>[] = [
+        {
+            name: (
+                <div style={{fontWeight: "bold", fontSize:"13px"}}>
+                    Name
+                </div>
+            ),
+            width: 'auto',
+            selector: (opr) => `${opr.firstName} ${opr.lastName}`,
+            sortable: true
+        },
+        {
+            name: (
+                <div style={{textAlign:"center", fontWeight: "bold", fontSize:"13px"}}>
+                    Ops Completed
+                </div>
+            ),
+            width: "200px",
+            selector: (opr) => opr.opsCompleted,
+            cell: (row) => (
+                <div style={{textAlign:"center", width:"50%"}}>
+                    {row.opsCompleted}
+                </div>
+            ),
+            sortable: true
+        },
+        {
+            name: (
+                <div style={{textAlign:"center", fontWeight: "bold", fontSize:"13px"}}>
+                    Reliability
+                </div>
+            ),
+            width: "150px",
+            selector: (opr) => opr.reliability,
+            cell: (row) => (
+                <div style={{width: "50%", textAlign:"center"}}>
+                    {(row.reliability * 100).toString()+'%'}
+                </div>
+            ),
+            sortable: true
+        },
+        {
+            name: (
+                <div style={{width: "100%", textAlign:"center", fontWeight: "bold", fontSize:"13px"}}>
+                    Endorsements
+                </div>
+            ),
+            width: "500px",
+            cell: (row) => (
+                <div style={{display: "flex", width: "100%"}}>
+                    {row.endorsements.map((msg:string, idx:number)=>(
+                        <Chip key={idx} label={msg}></Chip>
+                    ))}
+                </div>
+            )
+        },
+        {
+            name: (
+                <div style={{width: "100%", textAlign:"center", fontWeight: "bold", fontSize:"13px"}}>
+                    Check In/Out
+                </div>
+            ),
+            width: "150px",
+            cell: (row) => (
+                <div style={{width: "100%", textAlign:"center"}}>
+                    <Button variant="outlined" color={workerCheckIn[row.id]?.buttonColor ? "error" : "success"} onClick={() => handleCheckIn(row.id)} disabled={Boolean(workerCheckIn[row.id]?.checkIn && !!workerCheckIn[row.id]?.checkOut)}>
+                        {!workerCheckIn[row.id]?.checkIn ? "Check In" : !workerCheckIn[row.id]?.checkOut ? "Check Out" : "Recorded"}
+                    </Button>
+                </div>
+            )
+        }
+    ]
+
+
     return(
         <div>
             <ThemeProvider theme={font}>
-            {op.map((opChoice) => (
-                <Card key={opChoice.opId} sx={{marginBottom: '3rem'}} >
+
+                {filterListings.map((opChoice) => (
+
+                <Card key={opChoice.opId} sx={{marginBottom: '3rem'}}>
                     <CardContent>
                         <Typography sx={{fontWeight: 'bold'}}>{opChoice.opTitle}</Typography>
                         <Typography>Public ID: {opChoice.publicId}</Typography>
                         <Typography>Operators Needed: {opChoice.operatorsNeeded}</Typography>
                         <Typography>Start Time: {dateFormat(opChoice.startTime)}</Typography>
                         <Typography>End Time: {dateFormat(opChoice.endTime)}</Typography>
-
-                        <TableContainer sx={{marginTop: '2rem'}}>
-
-                            <Table sx={{border: '1px solid lightgrey', borderBottom: '1px solid lightgrey'}}>
-                                <TableHead sx={{backgroundColor: 'lightgrey'}}>
-                                    <TableRow>
-                                        <TableCell sx={{fontWeight: 'bold'}} >Name</TableCell>
-                                        <TableCell sx={{fontWeight: 'bold'}} >Ops Completed</TableCell>
-                                        <TableCell sx={{fontWeight: 'bold'}} >Reliability</TableCell>
-                                        <TableCell align="center" sx={{fontWeight: 'bold'}} >Endorsements</TableCell>
-                                        <TableCell sx={{fontWeight: 'bold'}} >Check In/Out</TableCell>
-                                    </TableRow>
-                                </TableHead>
-                                <TableBody>
-                                    {opChoice.operators.map((opr) => (
-                                        <TableRow key={opr.id} hover>
-                                            <TableCell>{opr.firstName + " " + opr.lastName}</TableCell>
-                                            <TableCell align="center">{opr.opsCompleted}</TableCell>
-                                            <TableCell align="center">{(opr.reliability * 100).toString()+"%"}</TableCell>
-                                            <TableCell>
-                                                {opr.endorsements.map((msg, idx)=>(
-                                                    <Chip key={idx} label={msg}/>
-                                                ))}
-                                            </TableCell>
-                                            <TableCell>
-                                                <Button variant="outlined" onClick={() => handleCheckIn(opr.id)} disabled={Boolean(workerCheckIn[opr.id]?.checkIn && !!workerCheckIn[opr.id]?.checkOut)}>
-                                                    {!workerCheckIn[opr.id]?.checkIn ? "Check In" : !workerCheckIn[opr.id]?.checkOut ? "Check Out" : "Recorded"}
-                                                </Button>
-                                            </TableCell>
-                                        </TableRow>
-                                    ))}
-                                </TableBody>
-                            </Table>
-                        </TableContainer>
+                        <div className="dataTable">
+                            <DataTable
+                                columns={cols}
+                                data={opChoice.operators}
+                                highlightOnHover
+                                customStyles={{
+                                    headRow:{
+                                        style:{
+                                            backgroundColor: "lightgrey",
+                                            justifyContent: "center",
+                                            gap: '0.1rem'
+                                        }
+                                    }
+                                }}
+                            />
+                        </div>
                     </CardContent>
                 </Card>
 
             ))}
+
             </ThemeProvider>
         </div>
     );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,38 +1,65 @@
-import { Stack, Typography } from "@mui/material";
+/**
+ * Ameya Telang -- Veryable FrontEnd Challenge
+ */
+
+"use client"
+
+import {Box, Stack, TextField, Typography } from "@mui/material";
+import { WavyBackground } from "@/components/ui/wavy-background";
 import GetOp from "@/app/op";
 import "./main.css";
+import logo from './logo.png';
+import Image from "next/image";
+import {useState} from "react";
 
 export default function Home() {
-  return (
-    <div className="background">
-        <Typography sx={{
-            fontFamily: 'Montserrat, sans-serif',
-            fontWeight: 'normal',
-            fontSize: '2rem',
-            marginLeft: '2rem',
-            paddingTop: '1rem',
-        }}>
-            VERYABLE
-        </Typography>
+
+    const [filter, setFilter] = useState("");
+
+    return (
+    <div>
+        <WavyBackground className="fixed w-full">
+        <Image src={logo} alt="logo" style={{
+            width: "15rem",
+            marginTop: '1rem',
+            marginLeft: '2rem'
+        }}/>
         <Stack
             sx={{
-            width: "100%",
-            height: "100vh",
-            justifyContent: "center",
-            alignItems: "center"
+                width: "100%",
+                height: "100vh",
+                justifyContent: "center",
+                alignItems: "center",
+                marginTop: "-5rem"
             }}
         >
+        <Box sx={{
+            display: "flex",
+            marginBottom: "2rem"
+        }}>
             <Typography sx={{
                 fontFamily: 'Montserrat, sans-serif',
                 fontWeight: 'thin',
                 fontSize: '1.5rem',
-                marginBottom: '2rem',
                 textDecoration: 'underline'
                 }}>
                     OP LISTINGS
+
             </Typography>
-            <GetOp/>
+            <TextField
+                size="small"
+                label="Filter listings..."
+                value={filter}
+                onChange={(event)=>setFilter(event.target.value)}
+                sx={{
+                    marginLeft: "53em"
+                }}
+            />
+        </Box>
+
+            <GetOp filter={filter}/>
         </Stack>
+        </WavyBackground>
     </div>
   );
 }
